1f2e443fb01dd0f8b5765a4e971bcdc90a2890be
Bruna Danyara <66446106+brunadanyara@users.noreply.github.com>
2023-02-23 16:46:35 -0300

    Add files via upload

---
+++ atv5_FuncionarioAndCarro/src/main/java/atividade3_PSII/Carro.java
@@ -0,0 +1,18 @@
+package atividade3_PSII;
+
+class Carro {
+        public String marca;
+        public String modelo;
+        public int anoFabricacao;
+        public int anoModelo;
+        public String cor;
+
+        public void imprimirInformacoes(){
+            System.out.println("Marca: "+this.marca);
+            System.out.println("Modelo: "+this.modelo);
+            System.out.println("Ano de Fabricação: "+this.anoFabricacao);
+            System.out.println("Ano do Modelo: "+this.anoModelo);
+            System.out.println("Cor: "+this.cor);
+        }
+    }
+}
---
+++ atv5_FuncionarioAndCarro/src/main/java/atividade3_PSII/Principal.java
@@ -0,0 +1,17 @@
+package atividade3_PSII;
+
+public class Principal {
+
+        public static void main(String args[]) {
+
+            Carro meuCarro;
+            meuCarro.marca = "BMW";
+            meuCarro.modelo = "525i";
+            meuCarro.anoFabricacao = 1992;
+            meuCarro.anoModelo = 1992;
+
+            meuCarro.imprimirInformacoes();
+        }
+    }
+
+}
